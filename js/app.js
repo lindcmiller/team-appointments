8eda452d0694c20e21cc3988c7fb342392326a6d
Lindsey Miller <lindc.miller@gmail.com>
2015-06-24 18:24:44 -0400

    add date/time picker, format plus icon and other views

--- js/app.js
+++ js/app.js
@@ -1,65 +1,99 @@
 var app = {};
 
-var arr = [];
+var data = [];
 
-     var Router = Backbone.Router.extend({
-       routes:{
-         '':'home',
-         'new':'new',
-         'detail':'detail',
-         'edit/:id':'edit',
-       },
+var Router = Backbone.Router.extend({
+      routes: {
+            '': 'home',
+            'new': 'new',
+            'detail/:id': 'detail',
+            'edit/:id': 'edit',
+      },
 
-       home: function() {
-         //myHomeFunction();
-         
-         var compiled = _.template($('#all-appts').html(), {variable: 'm'});
-          $('.main-content').html(compiled(arr[0]));
+      home: function () {
+            //myHomeFunction()
+
+            //Function that pulls in template "all-appts" into the HTML and passes it variable M
+            var compiled = _.template($('#all-appts').html(), { variable: 'm' });
+            //Takes the complied array and displays it in the Main Content div
+            $('.main-content').html(compiled(data));
+      },
+
+      new: function () {
+            //displays form
+            $('.main-content').html($('#create-appt').html());
+
+            $(".date").pickadate({
+                  format: "mm/dd/yyyy"
+            });
             
-         //
-         
-       },
+            $(".time").pickatime({
+                  interval: 30,
+                  format: 'h:i A',
+            });
 
-       new: function() {
-         //myNewFunction() 
-         $('.main-content').html($('#create-appt').html());
-         $('.new-appt').on('submit', function (e) {
-              //console.log(1);
-              e.preventDefault();
-              //console.log(2);
-          var appt = {}; 
-         
-            appt.title = $('.title').val();
-            appt.date = $('.date').val();
-            appt.time = $('.time').val();
-            appt.street = $('.street').val();
-            appt.city = $('.city').val();
-            appt.state = $('.state').val();
-              
-            arr.push(appt);
-            console.log(arr);
-            
-           //this.navigate('#all-appts', true);
-            
-           window.location.hash = '';
-         });
-       
-       },
+            $('.new-appt').on('submit', function (e) {
+                  e.preventDefault();
 
-      // detail: function() {
+                  var appt = {};
 
+                  appt.title = $('.title').val();
+                  appt.date = $('.date').val();
+                  appt.time = $('.time').val();
+                  appt.street = $('.street').val();
+                  appt.city = $('.city').val();
+                  appt.state = $('.state').val();
+
+                  data.push(appt);
+                  console.log(data);
+
+                  // this.navigate('#all-appts', true);
+                  window.location.hash = '';
+                  // app.instance.goto(view);
+
+            })
+      },
+
+
+      // detail: function () {
+      //       $('.main-content').html($('#detail').html());
+      //       <%- m.forEach(function (appt, i) { %>
+      //             <ul class="list" >
+      //                   <li class="title">
+      //                   <a href="#detail/<%- i %>">
+      //                   <%- appt.title %>
+      //                   </a>
+      //                   </li>
+      //                   <li class="date">
+      //                   <%- appt.date %>
+      //                   </li>
+      //                   <li class="time">
+      //                   <%- appt.time %>
+      //                   </li>
+      //                   <li class="street">
+      //                   <%- appt.street %>
+      //                   </li>
+      //                   <li class="city">
+      //                   <%- appt.city %>
+      //                   </li>
+      //                   <li class="state">
+      //                   <%- appt.state %>
+      //                   </li>
+      //                   </ul>
+      //                   <% }) %>
 
       // },
 
-     //  edit: function(id) {
-     //    alert("Put templates Here??" + id);
-     //  },
-     });
+      edit: function (id) {
+            $('.main-content').html($('#create-appt').html());
+            // alert("Put templates Here??" + id);
+      },
+});
 
 
-     var app_router = new Router;
-     // app_router.on("route:new", function(){
-     //   alert("Put templates Here??");
-     // });
+var app_router = new Router();
+// app_router.on("route:new", function(){
+//   alert("Put templates Here??");
+// });
 
-     Backbone.history.start();
+Backbone.history.start();
